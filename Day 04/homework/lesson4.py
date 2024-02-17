d9019767e26b428f21990f0446e96714ccace182
dachi2010 <dachidachie@gmail.com>
2024-02-17 13:52:29 +0300

    second commit

---
+++ Day 04/homework/lesson4.py
@@ -0,0 +1,3 @@
+me = 20
+myfriend = 30
+print(me + myfriend + 10)
---
+++ Day 05/homeworck/lesson5.py
@@ -0,0 +1,7 @@
+variable1 = 10
+variable2 = 5
+
+print(variable1, "", variable2, "=", variable1 * variable2)
+print(variable1, "/", variable2, "=", variable1 / variable2)
+print(variable1, "+", variable2, "=", variable1 + variable2)
+print(variable1, "-", variable2, "=", variable1 - variable2)
