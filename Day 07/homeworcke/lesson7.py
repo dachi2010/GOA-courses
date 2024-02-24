bcf3ed7521dff8f080f07637f44bf6feac45ab90
dachi2010 <dachidachie@gmail.com>
2024-02-24 13:47:25 +0300

    second commit

--- Day 07/homeworcke/lesson7.py
+++ Day 07/homeworcke/lesson7.py
@@ -1,16 +1,9 @@
-s1 = "Hello"
-s2 = "World"
-concatenated_string = (s1 + " " + s2)
-print("Concatenated string:", concatenated_string)
+# 2) დღეს ნასწავლი built in class'ები გატესტეთ. (str, float, int)
+name = "dachi"
+print(type(name))
 
-# Testing the float class
-f1 = 3.14
-f2 = 2.71
-sum_of_floats = f1 + f2
-print("Sum of floats:", sum_of_floats)
+age = 15
+print(type(age))
 
-# Testing the int class
-i1 = 10
-i2 = 5
-quotient_of_ints = i1 // i2
-print("Quotient of integers:", quotient_of_ints)
+value = 10.1
+print(type(value))
